f9282645164b45f2f99927367bcb161312849282
tedr56 <tedr56@gmail.com>
2013-12-02 09:41:58 +0100

    Update link detection

--- library.js
+++ library.js
@@ -2,11 +2,11 @@
 	"use strict";
 
 	var Dailymotion = {},
-		embed = '<iframe frameborder="0" width="480" height="270" src="http://www.dailymotion.com/embed/video/$1"></iframe>';
+		embed = '<iframe frameborder="0" width="480" height="270" src="http://www.dailymotion.com/embed/$1"></iframe>';
 
 	Dailymotion.parse = function(postContent, callback) {
 		// modified from http://stackoverflow.com/questions/7168987/
-		postContent = postContent.replace(/<a href="(?:https?:\/\/)?(?:www\.)?(?:dailymotion\.com)\/?(.+)<\/a>/g, embed);
+		postContent = postContent.replace(/<a href="(?:https?:\/\/)?(?:www\.)?(?:dailymotion\.com)\/?(?:video)\/?(.+)<\/a>/g, embed);
 		callback(null, postContent);
 	};
 
