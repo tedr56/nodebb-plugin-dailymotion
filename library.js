503ef07ee6ae051d91d8c9f88914a7d39a712bb2
tedr56 <tedr56@gmail.com>
2013-12-02 09:43:50 +0100

    missing video folder in embed link

--- library.js
+++ library.js
@@ -2,7 +2,7 @@
 	"use strict";
 
 	var Dailymotion = {},
-		embed = '<iframe frameborder="0" width="480" height="270" src="http://www.dailymotion.com/embed/$1"></iframe>';
+		embed = '<iframe frameborder="0" width="480" height="270" src="http://www.dailymotion.com/embed/video/$1"></iframe>';
 
 	Dailymotion.parse = function(postContent, callback) {
 		// modified from http://stackoverflow.com/questions/7168987/
