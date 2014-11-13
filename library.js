28c90a424e32a88e6d61be0ff5e6fc85448ba014
tedr56 <tedr56@gmail.com>
2014-11-13 18:58:17 +0100

    Fix library.js

--- library.js
+++ library.js
@@ -2,9 +2,10 @@
 	"use strict";
 
 	var Dailymotion = {},
-		embed = '<iframe class="dailymotion-embed" frameborder="0" width="480" height="270" src="http://www.dailymotion.com/embed/video/$1"></iframe>';
+		embed = '<iframe class="dailymotion-embed" frameborder="0" width="480" height="270" src="http://www.dailymotion.com/embed/video/$1"></iframe>'
 
     var embedUrl = /<a href="(?:https?:\/\/)?(?:www\.)?(?:dailymotion\.com)\/?(?:video)\/?(.+)<\/a>/g;
+    
 	Dailymotion.parse = function(data, callback) {
 	    if (!data || !data.postData || !data.postData.content) {
             return callback(null, data);
