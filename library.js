4140ae7355acfb4bf7b9c58a12acde7208e289ce
tedr56 <tedr56@gmail.com>
2014-11-13 19:11:55 +0100

    Fix library.js

--- library.js
+++ library.js
@@ -4,7 +4,7 @@
 	var Dailymotion = {},
 		embed = '<iframe class="dailymotion-embed" frameborder="0" width="480" height="270" src="http://www.dailymotion.com/embed/video/$1"></iframe>'
 
-    var embedUrl = /<a href="(?:https?:\/\/)?(?:www\.)?(?:dailymotion\.com)\/?(?:video)\/?(.+)"><\/a>/g;
+    var embedUrl = /<a href="(?:https?:\/\/)?(?:www\.)dailymotion\.com\/video\/(.+)">.+<\/a>/g;
     
 	Dailymotion.parse = function(data, callback) {
 	    if (!data || !data.postData || !data.postData.content) {
