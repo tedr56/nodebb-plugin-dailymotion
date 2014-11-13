247a13795deae3b56b7d1f72db836d4c9254563c
tedr56 <tedr56@gmail.com>
2014-11-13 18:30:49 +0100

    Update to 0.2.0 - NodeBB 0.6.0 Compatibility

--- library.js
+++ library.js
@@ -4,10 +4,16 @@
 	var Dailymotion = {},
 		embed = '<iframe class="dailymotion-embed" frameborder="0" width="480" height="270" src="http://www.dailymotion.com/embed/video/$1"></iframe>';
 
-	Dailymotion.parse = function(postContent, callback) {
-		// modified from http://stackoverflow.com/questions/7168987/
-		postContent = postContent.replace(/<a href="(?:https?:\/\/)?(?:www\.)?(?:dailymotion\.com)\/?(?:video)\/?(.+)<\/a>/g, embed);
-		callback(null, postContent);
+    var embedUrl = /<a href="(?:https?:\/\/)?(?:www\.)?(?:dailymotion\.com)\/?(?:video)\/?(.+)<\/a>/g;
+	Dailymotion.parse = function(data, callback) {
+	    if (!data || !data.postData || !data.postData.content) {
+            return callback(null, data);
+        }
+        
+        if (data.postData.content.match(embedUrl)) {
+           data.postData.content = data.postData.content.replace(embedUrl, embed);
+        }
+		callback(null, data);
 	};
 
 	module.exports = Dailymotion;
